37e71c5e85d35567c4e498b8dbc6e20512bd6406
Legendstarrx <legendstarr2024@gmail.com>
2025-04-09 13:45:47 +0100

    Update modified files, add new files, and remove deleted ones

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  rules: {
+    // Add any specific rules here
+    '@next/next/no-img-element': 'off',
+    'react/no-unescaped-entities': 'off'
+  }
+} 
--- next.config.js
+++ next.config.js
@@ -30,6 +30,32 @@ const nextConfig = {
         }
       });
     }
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 244000,
+          minChunks: 1,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          automaticNameDelimiter: '~',
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true
+            }
+          }
+        }
+      };
+    }
     return config;
   },
   // Security headers
