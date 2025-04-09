f98af825611b975d24c46c3f39b11656825a2b7f
Legendstarrx <legendstarr2024@gmail.com>
2025-04-09 15:01:58 +0100

    Update modified files, add new files, and remove deleted ones

--- utils/firebase-config.js
+++ utils/firebase-config.js
@@ -2,7 +2,6 @@ export const getFirebaseConfig = () => {
   const requiredVars = [
     'NEXT_PUBLIC_FIREBASE_API_KEY',
     'NEXT_PUBLIC_FIREBASE_PROJECT_ID',
-    'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'
   ];
 
   requiredVars.forEach(varName => {
@@ -13,7 +12,7 @@ export const getFirebaseConfig = () => {
 
   return {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "www.scriptsea.com",
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
