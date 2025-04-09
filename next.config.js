21b7b255341e084c2e0ff64d4bfb70dcd78eb449
Legendstarrx <legendstarr2024@gmail.com>
2025-04-09 12:54:34 +0100

    Update modified files, add new files, and remove deleted ones

--- next.config.js
+++ next.config.js
@@ -1,7 +1,48 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false
-}
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL: 'https://scriptsea.com'
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ]
+      }
+    ];
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  images: {
+    domains: ['scriptsea.com'],
+    unoptimized: true
+  },
+  output: 'standalone',
+  productionBrowserSourceMaps: false,
+  experimental: {
+    optimizeCss: true,
+    scrollRestoration: true
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
