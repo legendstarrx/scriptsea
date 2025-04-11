08526e21fe5ecf9211f3dca1cbbe36a22cce6517
Legendstarrx <legendstarr2024@gmail.com>
2025-04-11 10:32:57 +0100

    Update modified files, add new files, and remove deleted ones

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false
+  swcMinify: false,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization'
+          }
+        ]
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
