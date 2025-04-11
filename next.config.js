2a95165c2702fd1303146a65358d4a6332144829
Legendstarrx <legendstarr2024@gmail.com>
2025-04-11 17:28:16 +0100

    Update modified files, add new files, and remove deleted ones

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp'
+            value: 'credentialless'
           },
           {
             key: 'Cache-Control',
