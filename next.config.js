560bddfe30173dfa84f40802248977d7e6ae6ac2
Legendstarrx <legendstarr2024@gmail.com>
2025-04-11 12:25:02 +0100

    Update modified files, add new files, and remove deleted ones

--- next.config.js
+++ next.config.js
@@ -41,6 +41,11 @@ const nextConfig = {
     // This ensures pages are not statically optimized
     // which can cause caching issues
     dynamicPartialRendering: true
+  },
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
   }
 }
 
