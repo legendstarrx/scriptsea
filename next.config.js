33efb1f9863da87b6e36090030bd0a0ec144f5a0
Legendstarrx <legendstarr2024@gmail.com>
2025-04-11 13:28:04 +0100

    Update modified files, add new files, and remove deleted ones

--- next.config.js
+++ next.config.js
@@ -26,13 +26,9 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'no-store, must-revalidate'
+            value: 'public, max-age=31536000, immutable',
           },
-          {
-            key: 'Pragma',
-            value: 'no-cache'
-          }
-        ]
+        ],
       }
     ];
   },
@@ -46,7 +42,17 @@ const nextConfig = {
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
