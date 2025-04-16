282f86c8ad2ed4faef04a953f234aa227091fa37
Legendstarrx <legendstarr2024@gmail.com>
2025-04-16 15:12:01 +0100

    Update modified files, add new files, and remove deleted ones

--- pages/api/admin/get-users.js
+++ pages/api/admin/get-users.js
@@ -1,4 +1,6 @@
 import { adminDb } from '../../../lib/firebaseAdmin';
+
+export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
