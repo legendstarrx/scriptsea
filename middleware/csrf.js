9a05ba9796962d0734e3e4b69724b00b6a6bf215
Legendstarrx <legendstarr2024@gmail.com>
2025-04-18 09:00:37 +0100

    Update modified files, add new files, and remove deleted ones

--- middleware/csrf.js
+++
@@ -1,5 +0,0 @@
-import { csrf } from 'next-csrf';
-
-export const { csrf: csrfMiddleware, setup: setupCSRF } = csrf({
-  secret: process.env.CSRF_SECRET
-}); 
--- middleware/validateRequest.js
+++
@@ -1,8 +0,0 @@
-export const validateRequest = (schema) => async (req, res, next) => {
-  try {
-    await schema.validate(req.body);
-    next();
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
-}; 
